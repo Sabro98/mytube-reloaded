d0f626f5653b266c3fed9e43a6a8c889fa095fbf
Sabro98 <inhyuk04@gmail.com>
2021-06-25 20:47:52 +0900

    init middleware

--- src/server.js
+++ src/server.js
@@ -4,13 +4,27 @@ const PORT = 4000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.send("<h1>Hi guest</h1>");
-});
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
 
-app.get("/login", (req, res) => {
-  return res.send("you are in login");
-});
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send("I am final ware");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+
+app.get("/", handleHome);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
