43844398621e22f43f2d37931fd21c582927cd0e
Sabro98 <inhyuk04@gmail.com>
2021-06-26 16:00:06 +0900

    add rocket to listening message

--- src/server.js
+++ src/server.js
@@ -15,6 +15,6 @@ app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
