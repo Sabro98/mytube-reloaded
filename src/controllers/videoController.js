077e75e18867c7766e3555163a2079ed24a07744
Sabro98 <inhyuk04@gmail.com>
2021-06-26 18:31:41 +0900

    use parameter in pug & passing it in .js

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,13 @@
-export const trending = (req, res) => res.render("home");
-export const edit = (req, res) => res.render("edit");
-export const see = (req, res) => res.render("watch");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+
+export const edit = (req, res) =>
+  res.render("edit", { pageTitle: "Edit", videoID: `${req.params.id}` });
+
+export const see = (req, res) =>
+  res.render("watch", { pageTitle: "Watch", videoID: `${req.params.id}` });
+
 export const search = (req, res) => res.send("Search Video");
+
 export const upload = (req, res) => res.send("Upload Video");
 
 export const remove = (req, res) => {
