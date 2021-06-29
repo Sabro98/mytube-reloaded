f520578bf3e3f9389ca752e7276344b9ad4f5717
Sabro98 <inhyuk04@gmail.com>
2021-06-29 20:59:55 +0900

    pass videos information to pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,44 +1,49 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createAt: "2 minutes ago",
+    views: 1,
+    id: 0,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Vodeo",
-      rating: 5,
-      comments: 2,
-      createAt: "2 minutes ago",
-      view: 59,
-      id: 1,
-    },
-    {
-      title: "Second Vodeo",
-      rating: 5,
-      comments: 2,
-      createAt: "2 minutes ago",
-      view: 59,
-      id: 2,
-    },
-    {
-      title: "Third Vodeo",
-      rating: 5,
-      comments: 2,
-      createAt: "2 minutes ago",
-      view: 59,
-      id: 3,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const edit = (req, res) =>
   res.render("edit", { pageTitle: "Edit", videoID: `${req.params.id}` });
 
-export const see = (req, res) =>
-  res.render("watch", { pageTitle: "Watch", videoID: `${req.params.id}` });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id];
+
+  return res.render("watch", {
+    pageTitle: `Watching ${video.title}`,
+    video,
+  });
+};
 
 export const search = (req, res) => res.send("Search Video");
 
 export const upload = (req, res) => res.send("Upload Video");
 
-export const remove = (req, res) => {
-  console.log(req.params);
-  res.send("Remove Video");
-};
+export const remove = (req, res) => res.send("Remove Video");
