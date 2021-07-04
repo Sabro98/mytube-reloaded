ecb83f101b5399b0c9a51b7cb1a3a4f739cc94e5
Sabro98 <inhyuk04@gmail.com>
2021-07-05 00:27:48 +0900

    find data at home

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,55 +1,27 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createAt: "2 minutes ago",
-    views: 1,
-    id: 0,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createAt: "2 minutes ago",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createAt: "2 minutes ago",
-    views: 59,
-    id: 2,
-  },
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
-  return res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos: [] });
 };
 
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id];
 
-  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+  return res.render("edit", { pageTitle: `Editing:` });
 };
 
 export const postEdit = (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
-  videos[id].title = title;
+
   return res.redirect(`/videos/${id}`);
 };
 
 export const watch = (req, res) => {
   const { id } = req.params; //equal with const id = req.params.id;
-  const video = videos[id];
 
   return res.render("watch", {
-    pageTitle: `Watching: ${video.title}`,
-    video, // equal with video: video
+    pageTitle: `Watching: `,
   });
 };
 
