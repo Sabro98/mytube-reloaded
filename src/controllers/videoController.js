dede0af01dbc406d9c65d283031fd5c449a9de5c
Sabro98 <inhyuk04@gmail.com>
2021-07-28 01:44:11 +0900

    add: save middleware

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,6 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
-  console.log(videos);
   return res.render("home", { pageTitle: "Home", videos: videos });
 };
 
@@ -20,19 +19,17 @@ export const getEdit = async (req, res) => {
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  const video = await Video.findById(id);
+  const video = await Video.exists({ _id: id });
 
   if (!video) {
     return res.render("404", { pageTitle: "404 NOT FOUND" });
   }
 
-  video.title = title;
-  video.description = description;
-  video.hashtags = hashtags
-    .split(",")
-    .map((word) => (word.startsWith("#") ? word.trim() : `#${word.trim()}`));
-
-  await video.save();
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags,
+  });
 
   return res.redirect(`/videos/${id}`);
 };
@@ -62,11 +59,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags
-        .split(",")
-        .map((word) =>
-          word.startsWith("#") ? word.trim() : `#${word.trim()}`
-        ),
+      hashtags,
     });
     return res.redirect("/");
   } catch (error) {
--- src/models/Video.js
+++ src/models/Video.js
@@ -2,13 +2,19 @@ import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLength: 80 },
-  description: { type: String, required: true, trim: true, minLength: 20 },
+  description: { type: String, required: true, trim: true },
   hashtags: [{ type: String, trim: true }],
   createdAt: { type: Date, required: true, default: Date.now },
   meta: {
     views: { type: Number, default: 0 },
     rating: { type: Number, default: 0 },
   },
+});
+
+videoSchema.pre("save", async function () {
+  this.hashtags = this.hashtags[0]
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word.trim() : `#${word.trim()}`));
 });
 
 const Video = mongoose.model("Video", videoSchema);
