75eb97bd941aad73b20f6d7d4dc267a9ddebd186
Sabro98 <inhyuk04@gmail.com>
2021-08-06 02:39:22 +0900

    add: login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -9,7 +9,7 @@ export const postJoin = async (req, res) => {
   const pageTitle = "Join";
   if (password !== password2) {
     return res.status(400).render("join", {
-      pageTitle: pageTitle,
+      pageTitle,
       errorMessage: "Password not match.",
     });
   }
@@ -17,24 +17,48 @@ export const postJoin = async (req, res) => {
   const userExists = await User.exists({ $or: [{ username }, { email }] });
   if (userExists) {
     return res.status(400).render("join", {
-      pageTitle: pageTitle,
+      pageTitle,
       errorMessage: "User already Exist",
     });
   }
 
-  await User.create({
-    name,
-    email,
-    username,
-    password,
-    location,
-  });
+  try {
+    await User.create({
+      name,
+      email,
+      username,
+      password,
+      location,
+    });
 
-  return res.redirect("/login");
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: error._message,
+    });
+  }
+};
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+
+  //check if account exists
+  const exists = await User.exists({ username });
+  if (!exists) {
+    return res.status(400).render("login", {
+      pageTitle: "Login",
+      errorMessage: "Account does not exists",
+    });
+  }
+  //check if password correct
+  res.send("Login User");
 };
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Delete User");
-export const login = (req, res) => res.send("Login User");
 export const logout = (req, res) => res.send("Logout User");
 export const see = (req, res) => res.send("See User");
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { getJoin, postJoin, login } from "../controllers/userController";
+import {
+  getJoin,
+  postJoin,
+  getLogin,
+  postLogin,
+} from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 
 const rootRounter = express.Router();
 
 rootRounter.get("/", home);
 rootRounter.route("/join").get(getJoin).post(postJoin);
-rootRounter.get("/login", login);
+rootRounter.route("/login").get(getLogin).post(postLogin);
 rootRounter.get("/search", search);
 
 export default rootRounter;
