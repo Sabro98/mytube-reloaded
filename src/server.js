360d895092d991323955d0151110db099ca06019
Sabro98 <inhyuk04@gmail.com>
2021-08-07 02:16:19 +0900

    add: session

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRounter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -12,6 +13,17 @@ app.set("views", `${process.cwd()}/src/views`);
 
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "Hello~!~!~!~!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.get("/add-one/", (req, res, next) => {
+  return res.send(`${req.session.id}`);
+});
 
 app.use("/", rootRounter);
 app.use("/videos", videoRouter);
