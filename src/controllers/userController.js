4a2b36819b6e825b56158d992cdfea2e2a904f7f
Sabro98 <inhyuk04@gmail.com>
2021-08-09 02:11:20 +0900

    feat: session store in db

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -65,6 +65,7 @@ export const postLogin = async (req, res) => {
       errorMessage: "Wrong assword",
     });
   }
+
   req.session.loggedIn = true;
   req.session.user = user;
 
--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,5 @@
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Mytube";
-  console.log(res.locals);
   next();
 };
