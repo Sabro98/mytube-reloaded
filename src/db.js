75e13fca76b5fa4a9badba40e42376f9de5640ff
Sabro98 <inhyuk04@gmail.com>
2021-08-10 00:54:34 +0900

    feat: use .env

--- src/db.js
+++ src/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import process from "process";
 
-mongoose.connect("mongodb://127.0.0.1:27017/mytube", {
+mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
