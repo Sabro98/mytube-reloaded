91e44bf3051d5d7b95d8a1e45e67517061ec00ee
Sabro98 <inhyuk04@gmail.com>
2021-08-19 01:06:10 +0900

    feat: see user profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -306,6 +306,14 @@ export const postChangePassword = async (req, res) => {
   return res.redirect("/users/logout");
 };
 
-export const see = (req, res) => {
-  return res.send("See user");
+export const see = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
+  if (!user) {
+    return res.status(404).render("404", { pageTitle: "404 NOT FOUND" });
+  }
+  return res.render("users/profile", {
+    pageTitle: `${user.name}`,
+    user,
+  });
 };
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,6 +1,5 @@
 import express from "express";
 import {
-  edit,
   logout,
   startGithubLogin,
   finishGithubLogin,
@@ -8,9 +7,9 @@ import {
   finishKakaoLogin,
   getEdit,
   postEdit,
-  see,
   getChangePassword,
   postChangePassword,
+  see,
 } from "../controllers/userController";
 
 import {
@@ -20,6 +19,8 @@ import {
 } from "../middlewares";
 
 const userRouter = express.Router();
+
+userRouter.get("/:id([0-9a-f]{24})", see);
 
 userRouter
   .route("/edit")
@@ -39,7 +40,5 @@ userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get("/kakao/start", publicOnlyMiddleware, startKakaoLogin);
 userRouter.get("/kakao/finish", publicOnlyMiddleware, finishKakaoLogin);
-
-userRouter.get("/see", see);
 
 export default userRouter;
